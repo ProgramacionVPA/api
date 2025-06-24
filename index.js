3e3f6911180618ca9d4ae1d58d0410ca99606916
Victor Paul Alvarez Alvarez <VPALVAREZ@puce.edu.ec>
2025-06-24 17:01:14 -0500

    conexion mongo Atlas y compass

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 let tareasRoutes = require('./routes/tareas');
+let mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Conexión a MongoDB Atlas"))
+.catch((err) => console.error("Error al conectar a MongoDB:", err));
+
+//console.log(process.env.MONGO_URI);// Para verificar que la variable de entorno se carga correctamente
 
 
 app.use(express.json()); // Middleware para parsear JSON
